2b892415533bcd1d83c40bd54cf0e423b5d4c130
abhiram1206 <abhiramputta12@gmail.com>
2024-11-15 00:29:39 +0530

    11/15/2024

--- frontend/src/components/Headerbutton/Headerbutton.jsx
+++ frontend/src/components/Headerbutton/Headerbutton.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Headerbutton.css'
+import { useNavigate } from 'react-router-dom';
 
 const Headerbutton = () => {
+  const navigate = useNavigate();
+  const explore = () =>{
+    navigate('/product')
+  }
+
   return (
     <div className='header-button'>
-        <button className='exp-btn'>Explore Menu</button>
+      <button className='exp-btn' onClick={explore}>Explore Menu</button>
     </div>
   )
 }
